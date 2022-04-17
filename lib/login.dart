be7975dacb5eacfa88a827c8767d1087ccd4202e
Nicholas Tantama <72966311+waffle270202@users.noreply.github.com>
2022-04-17 21:14:43 +0800

    Update login

--- lib/login.dart
+++ lib/login.dart
@@ -19,33 +19,143 @@ class login extends StatefulWidget {
 }
 
 class loginState extends State<login> {
-  var _formKey = GlobalKey<FormState>();
+  var _formKeyuser = GlobalKey<FormState>();
+  var _formkeypass = GlobalKey<FormState>();
+  bool hidePassword = true;
   DatabaseReference ref = FirebaseDatabase.instance.ref('user');
 
   TextEditingController _fieldusername = TextEditingController();
   TextEditingController _fieldpassword = TextEditingController();
-  String username = "";
-  String password = "";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Expanded(
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+              child: Column(children: [
+                Text(
+                  "Go Blog",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+                ),
+              ]),
+            ),
+          ),
+          Container(
               child: Form(
-            key: _formKey,
-            child: Column(children: []),
-          ))
+            key: _formKeyuser,
+            child: Column(children: [
+              Container(
+                child: Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 0, 30, 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: _fieldusername,
+                          autocorrect: false,
+                          textAlign: TextAlign.left,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              prefixIcon: Icon(
+                                Icons.person,
+                                size: 35,
+                              ),
+                              labelText: 'Username'),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Masukan Username';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    )),
+              ),
+            ]),
+          )),
+          Container(
+              child: Form(
+                  key: _formkeypass,
+                  child: Column(children: [
+                    Container(
+                      child: Padding(
+                          padding: const EdgeInsets.fromLTRB(30, 0, 30, 10),
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                obscureText: hidePassword,
+                                controller: _fieldpassword,
+                                autocorrect: false,
+                                textAlign: TextAlign.left,
+                                decoration: InputDecoration(
+                                    suffixIcon: InkWell(
+                                      onTap: _togglePasswordview,
+                                      child: Icon(
+                                        Icons.visibility,
+                                      ),
+                                    ),
+                                    border: OutlineInputBorder(),
+                                    prefixIcon: Icon(
+                                      Icons.lock,
+                                      size: 35,
+                                    ),
+                                    labelText: 'Password'),
+                                validator: (value) {
+                                  if (value!.isEmpty) {
+                                    return 'Masukan Password';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ],
+                          )),
+                    )
+                  ]))),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Column(children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKeyuser.currentState!.validate()) ;
+                    if (_formkeypass.currentState!.validate()) ;
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: Size(400, 60),
+                      primary: Colors.black,
+                      onPrimary: Colors.white),
+                ),
+                TextButton(
+                    onPressed: validasi,
+                    child: Text('Already Have an account? Sign up',
+                        textAlign: TextAlign.right))
+              ]),
+            ),
+          )
         ]),
       ),
     );
   }
 
   void validasi() {
-    // username = _fieldusername.text;
-    // password = _fieldpassword.text;
     // validasiusername();
     // validasipassword();
     // ref.child("user").set({'Nama': 'Budi', 'Umur': '12'});
+  }
+
+  void _togglePasswordview() {
+    if (hidePassword == true) {
+      hidePassword = false;
+    } else {
+      hidePassword = true;
+    }
+    setState(() {});
   }
 }
